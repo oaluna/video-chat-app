ef317fa510dace676dd80fe3848e7a5fe068d9c7
Prerit Mathur <mathur.prerit@gmail.com>
2020-06-28 21:23:21 +0530

    added config files

---
+++ client/src/config/config.js
@@ -0,0 +1,5 @@
+const ENDPOINT="http://localhost:8080/"
+
+module.exports={
+    ENDPOINT,
+}
---
+++ client/src/config/urls.js
@@ -0,0 +1,15 @@
+export const urls={
+    login:{
+        getAllUsers:`/api/users/login`,
+    },
+    rooms:{
+        getUserRooms:'/api/groups',
+        addNewUser:'api/groups/adduser',
+    },
+    users:{
+        getOneuser:'/api/users'
+    },
+    messages:{
+        messagesByGroup:'/api/groupmessages',
+    }
+}
